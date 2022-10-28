44046e9e39e8ee85b34fda1ed465c264dc98c619
Ebruski <o.ebruski@gmail.com>
2022-10-28 02:08:51 +0100

    Array Examples implemented. ObjectMethods Examples implemented.

---
+++ Arrays/src/CharArray.java
@@ -0,0 +1,12 @@
+public class CharArray {
+    public static void main(String[] args) {
+        char c[] = {'a', 'z', 'q'};
+
+        if (c[0] == c[c.length - 1]){
+            System.out.println("First and Last elements are same");
+        }
+        else {
+            System.out.println("They are different");
+        }
+    }
+}
