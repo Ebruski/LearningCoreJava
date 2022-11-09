4fff7c1c1a21825d0ff87f6711cc45ce45d55955
Ebruski <o.ebruski@gmail.com>
2022-11-09 10:48:18 +0100

    FlightReservation App implemented. Java9Features with examples implemented.

---
+++ PatientAggrerator/src/main/java/Test.java
@@ -0,0 +1,5 @@
+public class Test {
+    public static void main(String[] args) {
+        System.out.println("Testing if this will work");
+    }
+}
---
+++ PatientClinicals/src/com/ebruski/pc/PatientClinicals.java
@@ -0,0 +1,12 @@
+package com.ebruski.pc;
+
+import com.ebruski.pr.PatientRegistration;
+
+public class PatientClinicals {
+
+    public static void main(String[] args) {
+        PatientRegistration registration = new PatientRegistration();
+        registration.getPaitentDetails();
+    }
+
+}
